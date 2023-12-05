dd0f85540e1298498037375e0c772f1debcadb90
iek30 <132556125+iek30@users.noreply.github.com>
2023-12-05 10:49:15 +0100

    l

--- src/app/global/global.component.ts
+++ src/app/global/global.component.ts
@@ -1,3 +1,4 @@
+// global.component.ts
 import { Component } from '@angular/core';
 import { PreguntasService } from '../preguntas.service';
 import { Pregunta } from '../pregunta';
@@ -9,16 +10,33 @@ import { Pregunta } from '../pregunta';
 })
 export class GlobalComponent {
 
-  pregunta:Pregunta = new Pregunta("",new Array(),"");
+  pregunta: Pregunta = new Pregunta("", new Array(), "");
+  respuestaSeleccionada: number = 0;
+  esCorrecta: boolean | undefined;
 
   constructor(private servicioPreguntas: PreguntasService) { }
 
   ngOnInit(): void {
     this.servicioPreguntas.getPreguntas().subscribe((data) => {
-        this.pregunta.pregunta = data.datos.pregunta;
-        this.pregunta.correcta = data.datos.respuesta_correcta;
-        this.pregunta.posibles = data.datos.opciones;
+      this.pregunta.pregunta = data.datos.pregunta;
+      this.pregunta.correcta = data.datos.respuesta_correcta;
+      this.pregunta.posibles[0] = data.datos.opciones[0];
+      this.pregunta.posibles[1] = data.datos.opciones[1];
+      this.pregunta.posibles[2] = data.datos.opciones[2];
+      this.pregunta.posibles[3] = data.datos.opciones[3];
     });
   }
 
+  actualizarRespuestaSeleccionada(index: number): void {
+    this.respuestaSeleccionada = index;
+  }
+
+  comprobarSiEsCorrecto(): void {
+    if (this.pregunta.correcta === this.pregunta.posibles[this.respuestaSeleccionada]) {
+      this.esCorrecta = true;
+    } else {
+      this.esCorrecta = false;
+    }
+  }
+  
 }
